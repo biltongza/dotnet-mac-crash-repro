7b37cd5c7744df78176e7bd358682b7cc56ad96e
Logan Dam <dam.loganza@gmail.com>
2021-12-06 22:10:16 +0200

    add repro case

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using static Darwin.CopyFile;
+
+using var state = new State();
+Console.WriteLine("created state");
+state.SetStatusCallback((Progress what, Stage stage, string source, string dest, State state) =>
+{
+    Console.WriteLine("got callback!!");
+    return NextStep.Continue;
+});
+Console.WriteLine("set status callback");
+
+var status = await Task.Run(() => Darwin.CopyFile.Copy("test.txt", "test2.txt", Flags.Clone, state));
+Console.WriteLine("Copied!");
+Console.WriteLine(status);
---
+++ README.md
@@ -0,0 +1,2 @@
+This repo serves to demonstrate a bug in .NET 6.0.100 on Mac running on an M1 chip.
+The program never runs successfully, and instead just exits with return code 0.
