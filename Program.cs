5f937e6c792344145b6b2301a26068e7bcfdac64
Logan Dam <dam.loganza@gmail.com>
2021-12-16 23:08:24 +0200

    switch to .net 5

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
 ﻿using static Darwin.CopyFile;
+using System;
+using System.Threading.Tasks;
 
-using var state = new State();
-Console.WriteLine("created state");
-state.SetStatusCallback((Progress what, Stage stage, string source, string dest, State state) =>
+namespace Crash
 {
-    Console.WriteLine("got callback!!");
-    return NextStep.Continue;
-});
-Console.WriteLine("set status callback");
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using var state = new State();
+            Console.WriteLine("created state");
+            state.SetStatusCallback((Progress what, Stage stage, string source, string dest, State state) =>
+            {
+                Console.WriteLine("got callback!!");
+                return NextStep.Continue;
+            });
+            Console.WriteLine("set status callback");
 
-var status = await Task.Run(() => Darwin.CopyFile.Copy("test.txt", "test2.txt", Flags.Clone, state));
-Console.WriteLine("Copied!");
-Console.WriteLine(status);
+            var status = await Task.Run(() => Darwin.CopyFile.Copy("test.txt", "test2.txt", Flags.Clone, state));
+            Console.WriteLine("Copied!");
+            Console.WriteLine(status);
+        }
+    }
+}
